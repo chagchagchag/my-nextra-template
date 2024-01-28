47872aeed70d9dea3ec33019b1318eff25376b90
chagchagchag <chagchagchag.dev@gmail.com>
2024-01-28 17:37:40 +0900

    next.config.js, theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,10 +4,7 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
+    link: 'https://github.com/chagchagchag/my-nextra-template',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
