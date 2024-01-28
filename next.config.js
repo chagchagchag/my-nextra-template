7e3fe9cf6ab70f45e8c239933a7583e9c0b527d3
chagchagchag <chagchagchag.dev@gmail.com>
2024-01-28 14:37:28 +0900

    package.json - gh-pages

--- next.config.js
+++ next.config.js
@@ -3,7 +3,24 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra({
-  distDir: 'dist',
-  basePath: '/my-nextra-template'
-})
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+  assetPrefix,
+  basePath: assetPrefix,
+  output: "export",
+}
+
+// module.exports = withNextra({
+//   // distDir: 'dist',
+//   // basePath: '/my-nextra-template'
+// })
+
+module.exports = {
+  ...withNextra,
+  ...nextConfig,
+}
